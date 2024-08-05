5a3b0f60e3447476e42c32810de08e6670a56f85
yunhao <xxxhuyh9819@gmail.com>
2024-08-05 15:54:03 -0500

    Updated the Question Component with animation

--- camu/src/pages/FAQ/FAQPage.jsx
+++ camu/src/pages/FAQ/FAQPage.jsx
@@ -1,35 +1,34 @@
 import styles from "./FAQPage.module.css";
 import SectionTitle from "../../components/SectionTitle.jsx";
+import Question from "../../components/FAQPage/Question.jsx";
 
 /**
  * An array of questions and answers. Answers are just placeholders now
  */
 const questionsAndAnswers = [
     {
-        question: 'What services do you offer?',
-        // answer: `We offer a comprehensive range of <strong>architectural</strong> services including conceptual design,
-        // 3D modeling, construction documentation, interior design, and project management. `
+        contents: 'What services do you offer?',
         answer: <p className={styles.answer}>We offer a comprehensive range of <strong>architectural services</strong> including conceptual design,
             3D modeling, construction documentation, <strong>interior design</strong>, and project management.
             Whether you&apos;re looking to building a new structure, renovate an existing space, or require sustainable
             design solutions, we have the expertise to meet your needs.</p>
     },
     {
-        question: 'How long does it take to complete a portfolio?',
+        contents: 'How long does it take to complete a portfolio?',
         answer: <p className={styles.answer}>We <strong>Offer</strong> consectetur adipiscing elit. Maecenas sed odio venenatis, aliquet nisi vel, pharetra dui.
             Mauris pretium lacus augue. Curabitur ultricies purus vel leo lacinia, in vestibulum lectus
             dignissim. Nullam rutrum et purus ac venenatis. Maecenas placerat posuere augue, eget hendrerit
             metus sodales a. Mauris et luctus est.</p>
     },
     {
-        question: 'What do I need to provide for my portfolio?',
+        contents: 'What do I need to provide for my portfolio?',
         answer: <p className={styles.answer}>We <strong>Offer</strong> consectetur adipiscing elit. Maecenas sed odio venenatis, aliquet nisi vel, pharetra dui.
             Mauris pretium lacus augue. Curabitur ultricies purus vel leo lacinia, in vestibulum lectus
             dignissim. Nullam rutrum et purus ac venenatis. Maecenas placerat posuere augue, eget hendrerit
             metus sodales a. Mauris et luctus est.</p>
     },
     {
-        question: 'Can you help me update my existing portfolio?',
+        contents: 'Can you help me update my existing portfolio?',
         answer: <p className={styles.answer}>We <strong>Offer</strong> consectetur adipiscing elit. Maecenas sed odio venenatis, aliquet nisi vel, pharetra dui.
             Mauris pretium lacus augue. Curabitur ultricies purus vel leo lacinia, in vestibulum lectus
             dignissim. Nullam rutrum et purus ac venenatis. Maecenas placerat posuere augue, eget hendrerit
@@ -50,14 +49,8 @@ export default function FAQPage() {
                     centered={false}
                 />
                 <section className={styles.questionListContainer}>
-                    {questionsAndAnswers.map((pair) => (
-                        <div key={pair.question} className={styles.qAndAContainer}>
-                            <div  className={styles.questionContainer}>
-                                <span className={styles.question}>{pair.question}</span>
-                                <img src='/src/assets/FAQs/plus.svg' alt='plus'/>
-                            </div>
-                            {pair.answer}
-                        </div>
+                    {questionsAndAnswers.map((question, index) => (
+                        <Question key={index} question={question} />
                     ))}
                 </section>
             </div>
