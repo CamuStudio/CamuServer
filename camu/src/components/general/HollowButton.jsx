d62ceb620b2c1cb22bf4a7a921f618b1fd82f416
yunhao <xxxhuyh9819@gmail.com>
2024-08-10 22:41:02 -0500

    Update: implemented the left part of the message box

--- camu/src/components/general/HollowButton.jsx
+++ camu/src/components/general/HollowButton.jsx
@@ -1,9 +1,9 @@
 import styles from './HollowButton.module.css'
 
-export default function HollowButton({text, children}) {
+export default function HollowButton({text, onClick, children}) {
     return (
         <button className={styles.hollowButton}>
-            <section className={styles.contents}>
+            <section className={styles.contents} onClick={onClick}>
                 <span className={styles.text}>{text}</span>
                 {children}
             </section>
--- camu/src/pages/Contact/ContactPage.jsx
+++ camu/src/pages/Contact/ContactPage.jsx
@@ -1,13 +1,37 @@
 import styles from './ContactPage.module.css';
 import SolidButton from "../../components/general/SolidButton.jsx";
-import {forwardRef} from "react";
+import {forwardRef, useState} from "react";
 import HollowButton from "../../components/general/HollowButton.jsx";
 
 export default forwardRef(function ContactPage(props, ref) {
+
+    const [showMessageBox, setShowMessageBox] = useState(false)
+
+    function toggleShowMessageBox() {
+        setShowMessageBox(!showMessageBox);
+    }
+
     return (
         <main className={styles.container} ref={ref}>
             <div className={styles.wrapper}>
-                <section className={styles.topContainer}>
+                {showMessageBox ? (
+                    <section className={styles.messageBoxContainer}>
+                        <section className={styles.leftContainer}>
+                            <img className={styles.backArrow}
+                                 src='/src/assets/general/arrow_forward.svg'
+                                 alt='back_arrow'
+                                 onClick={toggleShowMessageBox}
+                            />
+                            <p className={styles.subtitle}>Join the club</p>
+                            <h4 className={styles.title}>
+                                Talk to our advisor to see if Camu Design fits you!
+                            </h4>
+                        </section>
+                        <section className={styles.rightContainer}>
+                            <img src='/src/assets/contact/contact.png' alt='contact'/>
+                        </section>
+                    </section>
+                ) : <section className={styles.topContainer}>
                     <section className={styles.leftContainer}>
                         <p className={styles.subtitle}>Join the club</p>
                         <h4 className={styles.title}>
@@ -15,7 +39,7 @@ export default forwardRef(function ContactPage(props, ref) {
                         </h4>
                         <section className={styles.buttonsContainer}>
                             <SolidButton text={'Let\'s arrange a call'}/>
-                            <HollowButton text={'Send a message'}>
+                            <HollowButton text={'Send a message'} onClick={toggleShowMessageBox}>
                                 <img src='/src/assets/general/arrow_forward.svg' alt='arrow_forward'/>
                             </HollowButton>
                         </section>
@@ -23,7 +47,7 @@ export default forwardRef(function ContactPage(props, ref) {
                     <section className={styles.rightContainer}>
                         <img src='/src/assets/contact/contact.png' alt='contact'/>
                     </section>
-                </section>
+                </section>}
                 <section className={styles.linksContainer}>
                     <section className={styles.linkColumn}>
                         <p className={styles.subject}>What &apos;s it for</p>
