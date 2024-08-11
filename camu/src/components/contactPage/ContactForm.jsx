0e8d4da567bf9b524a6a5182ee731e2c7a412abc
yunhao <xxxhuyh9819@gmail.com>
2024-08-11 16:57:48 -0500

    Update: Implemented the basic structure of the interest form

--- camu/src/components/contactPage/ContactForm.jsx
+++ camu/src/components/contactPage/ContactForm.jsx
@@ -8,6 +8,7 @@ export default function ContactForm() {
 
     const [name, setName] = useState('');
     const [wechatId, setWechatId] = useState('');
+    const [showInterests, setShowInterests] = useState(false);
 
     /**
      * Function to handle submitting the form
@@ -21,6 +22,11 @@ export default function ContactForm() {
         setWechatId('');
     }
 
+    function handleShowInterest(e) {
+        e.preventDefault()
+        setShowInterests(!showInterests);
+    }
+
     return (
         <form className={styles.formContainer}>
             <section className={styles.titleContainer}>
@@ -31,18 +37,32 @@ export default function ContactForm() {
                 <InputField name={'Name'} value={name} onChange={(e) => setName(e.target.value)} />
                 <InputField name={'WeChat ID'} value={wechatId} onChange={(e) => setWechatId(e.target.value)} />
             </section>
-            <section className={styles.buttonsContainer}>
-                <div className={styles.buttonContainer}>
-                    <HollowButton text={'Tell us more'}>
-                        <div className={styles.plusContainer}>
-                            <img src='/src/assets/general/plus.svg' alt='plus'/>
-                        </div>
-                    </HollowButton>
-                </div>
-                <div className={name && wechatId ? styles.buttonContainer : styles.disabledButtonContainer}>
-                    <SolidButton text={'Submit'} onClick={handleSubmitForm}/>
-                </div>
-            </section>
+            {showInterests ? (
+                <section className={styles.interestContainer}>
+                    <h4 className={styles.interestTitle}>Interests</h4>
+                    <section className={styles.interests}>
+                        <span>Architecture design</span>
+                        <span>Architecture design</span>
+                        <span>Architecture design</span>
+                    </section>
+                    <section className={styles.submitButtonContainer}>
+                        <SolidButton text={'Submit'} handleSubmit={handleSubmitForm} />
+                    </section>
+                </section>
+            ) : (
+                <section className={styles.buttonsContainer}>
+                    <div className={styles.buttonContainer}>
+                        <HollowButton text={'Tell us more'} onClick={handleShowInterest}>
+                            <div className={styles.plusContainer}>
+                                <img src='/src/assets/general/plus.svg' alt='plus'/>
+                            </div>
+                        </HollowButton>
+                    </div>
+                    <div className={name && wechatId ? styles.buttonContainer : styles.disabledButtonContainer}>
+                        <SolidButton text={'Submit'} onClick={handleSubmitForm}/>
+                    </div>
+                </section>
+            )}
         </form>
     )
 }
