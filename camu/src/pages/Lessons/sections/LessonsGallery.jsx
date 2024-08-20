b22d22c4d99c74084215416d368632d3f0529b54
yunhao <xxxhuyh9819@gmail.com>
2024-08-20 14:29:17 -0500

    Update: Finished the basic structure of all lessons part

--- camu/src/pages/Lessons/sections/LessonsGallery.jsx
+++ camu/src/pages/Lessons/sections/LessonsGallery.jsx
@@ -1,6 +1,7 @@
 import styles from './LessonsGallery.module.css'
 import Lesson from "../../../components/Lessons/Lesson.jsx";
-import {latestLessons, topLatestLesson} from "../../../utils/dummyData.js";
+import {latestLessons, topLatestLesson, defaultCategories, allLessons} from "../../../utils/dummyData.js";
+import SearchBar from "../../../components/Lessons/SearchBar.jsx";
 
 export default function LessonsGallery() {
     return (
@@ -17,7 +18,7 @@ export default function LessonsGallery() {
                         </div>
                         <div className={styles.tagsContainer}>
                             {topLatestLesson.tags.map((tag, index) => (
-                                <div key={index} className={styles.tag}>Render</div>
+                                <div key={index} className={styles.tag}>{tag}</div>
                             ))}
                         </div>
                         <div className={styles.date}>
@@ -27,6 +28,38 @@ export default function LessonsGallery() {
                 </div>
                 <div className={styles.lessonsRow}>
                     {latestLessons.map((lesson, index) => (
+                        <div key={index} className={styles.lessonContainer}>
+                            <Lesson lesson={lesson}/>
+                        </div>
+                    ))}
+                </div>
+            </section>
+            <section className={styles.allLessonsContainer}>
+                <div className={styles.topContainer}>
+                    <div className={styles.topRow}>
+                        <h4>All Lessons(32)</h4>
+                        <div className={styles.searchBarContainer}>
+                            <SearchBar
+                                style={{borderRadius: 'var(--Corner-radius-Small, 12px)',
+                                border: '1px solid rgba(28, 27, 31, 0.40)'}}
+                                hasClearButton={false}
+                            />
+                        </div>
+                    </div>
+                    <div className={styles.categoryContainer}>
+                        <p>Category</p>
+                        <ul className={styles.categoryList}>
+                            {defaultCategories.map((category, index) => (
+                                <li key={index}>
+                                    <div key={index} className={styles.tag}>{category}</div>
+                                </li>
+                            ))}
+                            <img src='src/assets/Lessons/lessonsGallery/restart_alt.svg' alt='restart' />
+                        </ul>
+                    </div>
+                </div>
+                <div className={styles.allLessonsTable}>
+                    {allLessons.map((lesson, index) => (
                         <div key={index} className={styles.lessonContainer}>
                             <Lesson lesson={lesson}/>
                         </div>
--- camu/src/utils/dummyData.js
+++ camu/src/utils/dummyData.js
@@ -36,4 +36,32 @@ const latestLessons = [
     }
 ]
 
-export {latestLessons, topLatestLesson}
+// Dummy all lessons
+const allLessons = [...latestLessons,
+    {
+        previewImgSrc: 'src/assets/Lessons/lessonsGallery/latest_lesson_1.png',
+        title: 'What services do you offer?',
+        tags: ['Render', 'Render', 'Render'],
+        length: '24:34',
+        date: 'June 7, 2024',
+    },
+    {
+        previewImgSrc: 'src/assets/Lessons/lessonsGallery/latest_lesson_2.png',
+        title: 'What services do you offer?',
+        tags: ['Render', 'Render', 'Render'],
+        length: '24:34',
+        date: 'June 7, 2024',
+    },
+    {
+        previewImgSrc: 'src/assets/Lessons/lessonsGallery/latest_lesson_3.png',
+        title: 'What services do you offer?',
+        tags: ['Render', 'Render', 'Render'],
+        length: '24:34',
+        date: 'June 7, 2024',
+    }
+]
+
+
+const defaultCategories = ['Rhino', 'Rhino','Rhino','Rhino','Rhino']
+
+export {latestLessons, topLatestLesson, defaultCategories, allLessons}
