8909968a5b31453b7e4b47507f957fa5b4fe808c
yunhao <xxxhuyh9819@gmail.com>
2024-08-20 14:57:38 -0500

    Update: Updated the style of the Lesson Component

--- camu/src/components/Lessons/Lesson.jsx
+++ camu/src/components/Lessons/Lesson.jsx
@@ -1,23 +1,47 @@
 import styles from './Lesson.module.css';
 import {useState} from "react";
+import {motion} from "framer-motion";
+
+const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+};
 
 export default function Lesson({lesson}) {
 
     const {previewImgSrc, title, tags, length} = lesson;
+    const [hoveredIn, setHoveredIn] = useState(false)
 
     return (
         <div className={styles.container}>
-            <section className={styles.imageContainer}>
+            <section
+                className={hoveredIn ? styles.imageContainerHoveredIn : styles.imageContainer}
+                onMouseEnter={() => setHoveredIn(true)}
+                onMouseLeave={() => setHoveredIn(false)}
+            >
                 <img className={styles.previewImage} src={previewImgSrc} alt={title}/>
                 <section className={styles.lessonLengthContainer}>
                     <p>{length}</p>
                 </section>
-                <img className={styles.playIcon} src='src/assets/Lessons/lessonsGallery/play_arrow.svg' alt='play'/>
+                <motion.img
+                    className={styles.playIcon}
+                    src='src/assets/Lessons/lessonsGallery/play_arrow.svg'
+                    alt='play'
+                    animate={hoveredIn ? 'visible' : 'hidden'}
+                    variants={variants}
+                    transition={{duration: 0.3}}
+                    initial="hidden"
+                />
             </section>
             <section className={styles.moduleContainer}>
                 <div className={styles.titleContainer}>
                     <p className={styles.title}>{title}</p>
-                    <img src='src/assets/general/arrow_forward_black.svg' alt='enter'/>
+                    <img className={hoveredIn ? styles.forwardIconHoveredIn : styles.forwardIcon}
+                         src='src/assets/general/arrow_forward_black.svg'
+                         alt='enter'
+                         onMouseEnter={() => setHoveredIn(true)}
+                         onMouseLeave={() => setHoveredIn(false)}
+                    />
                 </div>
                 <div className={styles.tagsContainer}>
                     {tags.map((tag, index) => (
