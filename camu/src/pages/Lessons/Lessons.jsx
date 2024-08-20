9f323cf9b0f0ad7df7cad75c6f9b861fb0ccd9d2
yunhao <xxxhuyh9819@gmail.com>
2024-08-20 12:02:06 -0500

    Update: Renamed VideoGallery to LessonsGallery

--- camu/src/pages/Lessons/Lessons.jsx
+++ camu/src/pages/Lessons/Lessons.jsx
@@ -1,14 +1,14 @@
 import styles from './Lessons.module.css';
 import LessonsHero from "./sections/LessonsHero.jsx";
 import NavBar from "../../components/navbar/Navbar.jsx";
-import VideoGallery from "./sections/VideoGallery.jsx";
+import LessonsGallery from "./sections/LessonsGallery.jsx";
 
 export default function Lessons() {
     return (
         <div className={styles.wrapper}>
             <NavBar onScrollToSection={() => {console.log('haha')}} defaultSelection='Lessons'/>
             <LessonsHero />
-            <VideoGallery />
+            <LessonsGallery />
         </div>
     )
 }
