e792f215c55cd861e52491a3d5508b744d953680
yunhao <xxxhuyh9819@gmail.com>
2024-08-20 14:29:47 -0500

    Update: Added Contact section

--- camu/src/pages/Lessons/Lessons.jsx
+++ camu/src/pages/Lessons/Lessons.jsx
@@ -2,6 +2,7 @@ import styles from './Lessons.module.css';
 import LessonsHero from "./sections/LessonsHero.jsx";
 import NavBar from "../../components/navbar/Navbar.jsx";
 import LessonsGallery from "./sections/LessonsGallery.jsx";
+import ContactPage from "../Contact/ContactPage.jsx";
 
 export default function Lessons() {
     return (
@@ -9,6 +10,7 @@ export default function Lessons() {
             <NavBar onScrollToSection={() => {console.log('haha')}} defaultSelection='Lessons'/>
             <LessonsHero />
             <LessonsGallery />
+            <ContactPage />
         </div>
     )
 }
